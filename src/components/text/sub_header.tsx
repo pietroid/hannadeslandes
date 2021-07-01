5e468ee5ec96bc5f5a2285aa73f74a8769d93a4e
Pietro <pietrodomingues@gmail.com>
2021-07-01 18:49:34 -0300

    Refatorar para css e responsividade (mobile first)

--- src/components/text/sub_header.tsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import * as React from 'React';
-
-export const Header = styled.text`
-
-`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,29 @@
 import * as React from "react"
 import '../components/global_style.css'
-import Logo  from "../components/logo"
-import { Body } from "../components/text/body"
-import { Header } from "../components/text/header"
+
 import mapaAstral from '../images/mapa_astral.png'
+import logoImage from '../images/logo.svg'
+
 const IndexPage = () => {
   return (
     <>
-    <div style={{
-      display:'flex',
-      flex:1,
-      marginTop:50,
-      justifyContent:'center',
-    }}>
-    <Logo/>
+    <div className="header">
+      <img className="logo" src={logoImage}/>
     </div>
-
-    <div style={{margin:150}}>
-      
-      <div style={{
-        display:'flex',
-        flex:1,
-        justifyContent:'space-between',
-      }}>
-        
-        
-      <div style={{width:400}}>
-        <Header>Precisa de ajuda?</Header>
-      <div style={{height:40}}></div>
-        <Body>Você está com problemas na sua vida? Você não consegue mais lidar com eles? Se consulte para entender os motivos dos seus problemas e para descobrir como pode resolvê-los.</Body>
+      <div className="wrapper">
+        <div className="centered_wrapper">
+          <h1 className="centered_text_mobile">Precisa de ajuda?</h1>
+          <div className="v_space"/>
+          <p id="intro_text">Você está com problemas na sua vida? Você não consegue mais lidar com eles? Se consulte para entender os motivos dos seus problemas e para descobrir como pode resolvê-los.</p>
+        </div>
+        <div class = "center_spacing"/>
+        <div className="centered_wrapper">
+          <h1 className="centered_text" >Mapa Astral</h1>
+          <div className="service_image_wrapper">
+            <img src={mapaAstral} className="box"/>
+          </div>
+        </div>
       </div>
-      <div style={{flexDirection:'column',display:'flex',alignItems:'center'}}>
-        <Header>Mapa Astral</Header>
-        <img src={mapaAstral} style={{width:450}}></img>
-      </div>
-      </div>
-    </div>
     </>
   )
 }
